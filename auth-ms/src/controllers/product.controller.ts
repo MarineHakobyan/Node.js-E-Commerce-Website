d8e9a164701b81b41d44052326aace8ffba49030
MarineHakobyan <marine.hakobyan4649@gmail.com>
2024-03-14 14:45:39 +0400

    don't use product API for now

--- auth-ms/src/controllers/product.controller.ts
+++ auth-ms/src/controllers/product.controller.ts
@@ -1,34 +1,34 @@
-import { ProductService } from './services';
+// import { ProductService } from './services';
 
 export class ProductController {
-  private productService: ProductService;
-
-  constructor(productService: ProductService) {
-    this.productService = productService;
-  }
-
-  async createProduct(productData: any) {
-    const product = await this.productService.createProduct(
-      userId,
-      productData,
-    );
-    return product;
-  }
-
-  async getAllProducts() {
-    const products = await this.productService.getAllProducts();
-    return products;
-  }
-
-  async updateProduct(productId: number, productData: any) {
-    const product = await this.productService.updateProduct(
-      productId,
-      productData,
-    );
-    return product;
-  }
-
-  async deleteProduct(productId: number) {
-    await this.productService.deleteProduct(productId);
-  }
+  // private productService: ProductService;
+  //
+  // constructor(productService: ProductService) {
+  //   this.productService = productService;
+  // }
+  //
+  // async createProduct(productData: any) {
+  //   const product = await this.productService.createProduct(
+  //     userId,
+  //     productData,
+  //   );
+  //   return product;
+  // }
+  //
+  // async getAllProducts() {
+  //   const products = await this.productService.getAllProducts();
+  //   return products;
+  // }
+  //
+  // async updateProduct(productId: number, productData: any) {
+  //   const product = await this.productService.updateProduct(
+  //     productId,
+  //     productData,
+  //   );
+  //   return product;
+  // }
+  //
+  // async deleteProduct(productId: number) {
+  //   await this.productService.deleteProduct(productId);
+  // }
 }
--- auth-ms/src/services/product.service.ts
+++ auth-ms/src/services/product.service.ts
@@ -3,44 +3,44 @@ import { ProductEntity } from '../orm/entities/product.entity';
 import { UserEntity } from '../orm/entities/user.entity';
 
 export class ProductService {
-  private productRepository: Repository<ProductEntity>;
-  private userRepository: Repository<UserEntity>;
-
-  constructor() {
-    this.productRepository = getRepository(ProductEntity);
-    this.userRepository = getRepository(UserEntity);
-  }
-
-  async createProduct(userId: number, productData: any) {
-    const user = await this.userRepository.findOne(userId);
-
-    if (!user) {
-      throw new Error('User not found');
-    }
-
-    const product = new ProductEntity();
-    product.title = productData.title;
-    // product.user = user; // uncomment if you need user association
-    await this.productRepository.save(product);
-
-    return product;
-  }
-
-  async getAllProducts() {
-    return await this.productRepository.find({ relations: ['user'] });
-  }
-
-  async updateProduct(productId: number, productData: any) {
-    await this.productRepository.update(productId, productData);
-    return await this.productRepository.findOne(productId, {
-      relations: ['user'],
-    });
-  }
-
-  async deleteProduct(productId: number) {
-    const result = await this.productRepository.delete(productId);
-    if (!result.affected) {
-      throw new Error('Product not found or could not be deleted.');
-    }
-  }
+  // private productRepository: Repository<ProductEntity>;
+  // private userRepository: Repository<UserEntity>;
+  //
+  // constructor() {
+  //   this.productRepository = getRepository(ProductEntity);
+  //   this.userRepository = getRepository(UserEntity);
+  // }
+  //
+  // async createProduct(userId: number, productData: any) {
+  //   const user = await this.userRepository.findOne(userId);
+  //
+  //   if (!user) {
+  //     throw new Error('User not found');
+  //   }
+  //
+  //   const product = new ProductEntity();
+  //   product.title = productData.title;
+  //   // product.user = user; // uncomment if you need user association
+  //   await this.productRepository.save(product);
+  //
+  //   return product;
+  // }
+  //
+  // async getAllProducts() {
+  //   return await this.productRepository.find({ relations: ['user'] });
+  // }
+  //
+  // async updateProduct(productId: number, productData: any) {
+  //   await this.productRepository.update(productId, productData);
+  //   return await this.productRepository.findOne(productId, {
+  //     relations: ['user'],
+  //   });
+  // }
+  //
+  // async deleteProduct(productId: number) {
+  //   const result = await this.productRepository.delete(productId);
+  //   if (!result.affected) {
+  //     throw new Error('Product not found or could not be deleted.');
+  //   }
+  // }
 }
